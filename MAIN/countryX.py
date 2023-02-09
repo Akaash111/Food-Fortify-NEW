56e4126710c91fb4c9f836ba02fa0e80028a5e8f
Akaash111 <akaashb1403@gmail.com>
2023-02-09 13:53:34 +0800

    2

--- MAIN/countryX.py
+++ MAIN/countryX.py
@@ -1,35 +1,45 @@
 #data for country A
 import random
 import numpy as np
-
-class data_CountryX():
-
-    def __init__(self):
-        self.data = np.array([])
-
-        #data contains arrays of feature values for country A over the past 40 months
-
-        for _ in range(0, 40):
-
-            a1 = random.random()
-            a2 = random.random()
-            a3 = random.random()
-            a4 = random.random()
-            a5 = random.random()
-            a6 = random.random()
-
-            monthly_data = np.array([a1, a2, a3, a4, a5, a6])
-            data = np.append(self.data, monthly_data)
-
-            print(self.data)
-        
-        #calc reliability score for each year
-
-        for year in self.data:
-            print(year[3])
-            rel = (year[3] * year[5]) / (year[0] * year[1] * year[3] * year[5])
-
-            print(rel)
+import os
+import pickle
 
 
-init = data_CountryX()
+DATA = []
+REL_SCORES = []
+DATA_DIR = "/Users/akaash/Desktop/Projects/FF/ASSETS"
+
+#data contains arrays of feature values for country A over the past 40 months
+
+for _ in range(0, 40):
+
+    a1 = random.random()
+    a2 = random.random()
+    a3 = random.random()
+    a4 = random.random()
+    a5 = random.random()
+    a6 = random.random()
+
+    monthly_data = [a1, a2, a3, a4, a5, a6]
+
+    DATA.append(monthly_data)
+
+#print(DATA[0])
+
+#calc reliability score for each year
+
+for year in DATA:
+    
+    rel = (year[3] * year[5]) / (year[0] * year[1] * year[3] * year[5])
+
+    REL_SCORES.append(rel)
+
+print(REL_SCORES)
+
+pickle_out = open("X_train.pickle", "wb")
+pickle.dump(DATA, pickle_out)
+pickle_out.close()
+
+pickle_out = open("Y_train.pickle", "wb")
+pickle.dump(REL_SCORES, pickle_out)
+pickle_out.close()
--- MAIN/data_augmentation.py
+++ MAIN/data_augmentation.py
@@ -1,3 +1,19 @@
-#data for country A
-import random
+from matplotlib import pyplot as plt
+from sklearn import linear_model
+import pandas as pd
+import numpy as np
+import pickle
+
+
+pickle_in = open("/Users/akaash/Desktop/Projects/FF/ASSETS/X_train.pickle", "rb")
+X_train = pickle.load(pickle_in)
+
+pickle_in = open("/Users/akaash/Desktop/Projects/FF/ASSETS/Y_train.pickle", "rb")
+Y_train = pickle.load(pickle_in)
+
+
+
+
+
+
 
